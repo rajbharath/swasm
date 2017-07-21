257476aed5dda99d6a861ffaaa89f31860154931
rajbharath <rbrajbharath1@gmail.com>
2017-07-21 12:05:48 +0530

    Added active menu selection based on url

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  def menu_active?(menu_item_name)
+    case menu_item_name
+      when 'home'
+        current_page? root_path
+      when 'my_projects'
+        current_page? user_projects_path(current_user.id)
+      when 'volunteers'
+        current_page? volunteers_path
+    end
+  end
 end
